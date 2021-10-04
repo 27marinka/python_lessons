e79d705ca2f9521f6886acd82206f4b1008609dd
27marinka <hedgehogs@tut.by>
2021-10-04 12:29:00 +0300

    Hometask for lesson 1

---
+++ les1_task2.py
@@ -0,0 +1,21 @@
+"""
+2. Задание
+Даны данные человека
+"""
+import datetime
+
+name = "Василий"
+surname = "Иванов"
+age = 33
+height = 182
+
+now = datetime.datetime.now()
+current_year = now.year
+
+birth_year = current_year - age
+bio = name + ' ' + surname + ' ' + str(birth_year) + ' ростом ' + str(height) + ' см'
+print(bio)
+
+# вычислить Год рождения (относительно текущего года) и присвоить переменной birth_year
+# Составить строку по шаблону {name} {surname} {birth_year} ростом {height} см и присвоить переменной bio
+# Напечатать на экране значение bio
